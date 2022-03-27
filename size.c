98a9d6bcd2e4d7b13653fdb0dc5d9b818ad4dd03
suzy <choisuji1210@naver.com>
2022-03-27 23:35:10 +0900

    new file

--- size.c
+++ size.c
@@ -7,7 +7,7 @@ void main()
 
     printf("   [-----  [ChoiSuzy]  [2021024135]  -----]  \n\n"); 
 
-    printf("sizeof(x) = %lu\n", sizeof(x));         /* x의 사이즈 크기는 정수의 주소이기에 8bytes */
-    printf("sizeof(*x) = %lu\n", sizeof(*x));       /* *x의 사이즈 크기는 정수의 주소이기에 8bytes */
-    printf("sizeof(**x) = %lu\n", sizeof(**x));     /* **x의 사이즈 크기는 정수값이기에 4bytes */
+    printf("sizeof(x) = %lu\n", sizeof(x));         /* x의 사이즈 크기는 정수의 주소 */
+    printf("sizeof(*x) = %lu\n", sizeof(*x));       /* *x의 사이즈 크기는 정수의 주소 */
+    printf("sizeof(**x) = %lu\n", sizeof(**x));     /* **x의 사이즈 크기는 정수값 */
 }
